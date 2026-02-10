3b4a5678447d38e8f01dcc2372ec774fc4b2fa9a
ani737 <aniketshah1994@gmail.com>
2026-02-10 20:36:36 +0530

    Implement user profile retrieval and update endpoints

---
+++ server/src/controllers/userController.js
@@ -0,0 +1,19 @@
+const User = require('../models/User');
+
+exports.getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.status(200).json(updatedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
